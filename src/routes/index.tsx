d042515fae40c470b836bc617c789e1cac17a636
Wesley Carvalho <crafael.wesley@gmail.com>
2022-06-08 19:47:03 -0300

    Adicionado Roteamento

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,14 @@
-export { };
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+
+    return (
+        <Routes>
+            <Route path="/" element={<p>Página inicial</p>} />
+
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    );
+
+}
+
